ee341bf3aa3b444d2fc37dc72880a85d547f9414
Samuel Williams <samuelspexy@gmail.com>
2025-01-18 22:58:04 +0200

    added chat route to express

--- api/main.js
+++ api/main.js
@@ -2,11 +2,17 @@ import express from "express";
 import "dotenv/config";
 import { connectToCluster } from "./src/config/db.js";
 import { router as authRoute } from "./src/route/public/auth_route.js";
+import { router as chatRoute } from "./src/route/private/chat_route.js";
+import cookieParser from "cookie-parser";
 const PORT = process.env.PORT;
 const DB_URI = process.env.DB_URI;
+const BASE_URI = process.env.BASE_URI;
+console.log(BASE_URI);
 const app = express();
 app.use(express.json());
-app.use("/api/v1/auth", authRoute);
+app.use(cookieParser());
+app.use(`${BASE_URI}/auth`, authRoute);
+app.use(`${BASE_URI}/chat`, chatRoute);
 app.listen(PORT, async () => {
   let mongoClient;
   try {
