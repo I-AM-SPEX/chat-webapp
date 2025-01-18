c7a4d7da9882cc72fa6cf03760e440503f5e0bdc
Samuel Williams <samuelspexy@gmail.com>
2025-01-18 15:55:45 +0200

    FindUserByName function definiton

---
+++ api/src/config/db.js
@@ -0,0 +1,49 @@
+import { MongoClient } from "mongodb";
+const DB = process.env.DB;
+const USER_COLLECTION = process.env.USER_COLLECTION;
+const DB_URI = process.env.DB_URI;
+
+const getUsersCollection = (mongoClient) => {
+  const db = mongoClient.db(DB);
+  const collection = db.collection(USER_COLLECTION);
+  return collection;
+};
+const connectToCluster = async () => {
+  let mongoClient;
+  try {
+    mongoClient = new MongoClient(DB_URI);
+    console.log("Connecting to Cluster");
+    await mongoClient.connect();
+    console.log("Successfully connected to cluster");
+    return mongoClient;
+  } catch (error) {
+    console.error("Connection to cluseter failed"), error;
+    process.exit();
+  }
+};
+
+const addUserToDb = async (user) => {
+  let mongoClient;
+
+  try {
+    mongoClient = await connectToCluster(DB_URI);
+    const usersCollection = getUsersCollection(mongoClient);
+    await usersCollection.insertOne(user);
+  } catch (error) {
+    console.error("failed tod add user to db", error);
+  } finally {
+    await mongoClient.close();
+  }
+};
+
+const findUserByUserName = async (userName) => {
+  let mongoClient;
+
+  try {
+    mongoClient = await connectToCluster(DB_URI);
+    const usersCollection = getUsersCollection(mongoClient);
+    const user = await usersCollection.findOne({ userName: userName });
+    return user;
+  } catch (error) { }
+};
+export { connectToCluster, addUserToDb, findUserByUserName };
