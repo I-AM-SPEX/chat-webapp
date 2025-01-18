259d6458f8f79b873d74b017ef6bb119cbb53b8d
Samuel Williams <samuelspexy@gmail.com>
2025-01-18 17:13:26 +0200

    Login auth controller

--- api/src/controller/auth_controller.js
+++ api/src/controller/auth_controller.js
@@ -1,5 +1,5 @@
 import { findUserByUserName } from "../config/db.js";
-import { createUser } from "../service/auth_service.js";
+import { authenticateUser, createUser } from "../service/auth_service.js";
 
 const signUp = async (req, res) => {
   const { userName, password } = req.body;
@@ -13,11 +13,39 @@ const signUp = async (req, res) => {
         res.status(200).json({ message: "userName already exists." });
       }
     } else {
-      res.status(200).json({ message: "Fields cant be empty" });
+      res.status(200).json({ message: "Fields can not be empty" });
     }
   } catch (error) { }
 };
 
-const login = async (req, res) => { };
+const login = async (req, res) => {
+  const { userName, password } = req.body;
+  try {
+    if (userName && password) {
+      const user = await findUserByUserName(userName);
+      if (user) {
+        const result = await authenticateUser(password, user);
+        if (result.status) {
+          res
+            .status(200)
+            .cookie("refreshToken", result.tokens.refreshToken, {
+              httpOnly: true,
+              sameSite: "strict",
+            })
+            .header("Authorization", result.tokens.accessToken)
+            .json({ message: result.message });
+        } else {
+          res.status(200).json({ message: result.message });
+        }
+      } else {
+        res.status(200).json({ message: "You do not have an account" });
+      }
+    } else {
+      res.status(200).json({ message: "Fields can not be empty" });
+    }
+  } catch (error) {
+    console.log("Login Failed", error);
+  }
+};
 
 export { signUp, login };
