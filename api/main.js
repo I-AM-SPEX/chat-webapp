7a66fcc0ccc03fd748e575ebc44dd4feb1810d18
Samuel Williams <samuelspexy@gmail.com>
2025-01-18 13:24:57 +0200

    Express using express.json

--- api/main.js
+++ api/main.js
@@ -1,11 +1,19 @@
 import express from "express";
 import "dotenv/config";
 import { connectToCluster } from "./src/config/db.js";
-
+import { router as authRoute } from "./src/route/public/auth_route.js";
 const PORT = process.env.PORT;
 const DB_URI = process.env.DB_URI;
 const app = express();
-
+app.use(express.json());
+app.use("/api/v1/auth", authRoute);
 app.listen(PORT, async () => {
-  await connectToCluster(DB_URI);
+  let mongoClient;
+  try {
+    mongoClient = await connectToCluster(DB_URI);
+  } catch (error) {
+  } finally {
+    await mongoClient.close();
+    console.log("Connection closed succesfully");
+  }
 });
