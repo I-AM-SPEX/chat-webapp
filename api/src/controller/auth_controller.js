ba818f379ecfdeaf92dc5891465f7b7ec6be7fc9
Samuel Williams <samuelspexy@gmail.com>
2025-01-18 15:56:49 +0200

    userName already exists response added

--- api/src/controller/auth_controller.js
+++ api/src/controller/auth_controller.js
@@ -1,11 +1,17 @@
+import { findUserByUserName } from "../config/db.js";
 import { createUser } from "../service/auth_service.js";
 
 const signUp = async (req, res) => {
   const { userName, password } = req.body;
   try {
     if (userName && password) {
-      await createUser(userName, password);
-      res.status(200).json({ message: "Account created succesfully" });
+      const user = await findUserByUserName(userName);
+      if (!user) {
+        await createUser(userName, password);
+        res.status(200).json({ message: "Account created succesfully" });
+      } else {
+        res.status(200).json({ message: "userName already exists." });
+      }
     } else {
       res.status(200).json({ message: "Fields cant be empty" });
     }
