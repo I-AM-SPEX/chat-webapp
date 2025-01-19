59dfac151844f9ba8c39edcda4937cf5cb337e86
Samuel Williams <samuelspexy@gmail.com>
2025-01-19 22:08:09 +0200

    chatExist function to check if chat is already existing

--- api/src/service/chat_service.js
+++ api/src/service/chat_service.js
@@ -1,32 +1,63 @@
 import { ObjectId } from "mongodb";
-import { addChat, findUserByUserName } from "../config/db.js";
+import { addChat, findChatByChatId, findUserByUserName } from "../config/db.js";
 import { Chat } from "../model/chat.js";
 import { Message } from "../model/message.js";
 
 const dileverMessage = async () => { };
 
 const createChat = async (userName, friendUserName) => {
-  const friend = await findUserByUserName(friendUserName);
-  if (friend) {
-    const chatCreationNotification = `${userName} created this chat.`;
-    const user = await findUserByUserName(userName);
-    const chatId = `${user._id.toString()}_${friend._id.toString()}`;
-    const id = new ObjectId();
-    const chat = new Chat(id, chatId);
-    const message = new Message(
-      user._id,
-      new Date().toString(),
-      chatCreationNotification,
-    );
-    chat.chats.push(message);
-    await addChat(chat);
-    return { status: true, message: "Chat Created Successfully." };
-  } else {
-    return {
-      status: false,
-      message: "The user does not exist. Invite them to sign up for plain chat",
-    };
+  try {
+    const isChat = await chatExists(userName, friendUserName);
+    if (!isChat) {
+      const friend = await findUserByUserName(friendUserName);
+      if (friend) {
+        const chatCreationNotification = `${userName} created this chat.`;
+        const user = await findUserByUserName(userName);
+        const chatId = `${user._id.toString()}_${friend._id.toString()}`;
+        const id = new ObjectId();
+        const chat = new Chat(id, chatId);
+        const message = new Message(
+          user._id,
+          new Date().toString(),
+          chatCreationNotification,
+        );
+        chat.chats.push(message);
+        await addChat(chat);
+        return { status: true, message: "Chat Created Successfully." };
+      } else {
+        return {
+          status: false,
+          message:
+            "The user does not exist. Invite them to sign up for plain chat",
+        };
+      }
+    } else {
+      return {
+        status: false,
+        message: "You already have a chat with this user",
+      };
+    }
+  } catch (error) {
+    console.log("CreateChat function failed", error);
   }
 };
-
+const chatExists = async (userName, friendUserName) => {
+  try {
+    const user = await findUserByUserName(userName);
+    const friend = await findUserByUserName(friendUserName);
+    const comboOne = `${user._id.toString()}_${friend._id.toString()}`;
+    const comboTwo = `${friend._id.toString()}_${user._id.toString()}`;
+    if (await findChatByChatId(comboOne)) {
+      console.log("found chat");
+      return true;
+    } else if (await findChatByChatId(comboTwo)) {
+      return true;
+    } else {
+      console.log("did not find chat");
+      return false;
+    }
+  } catch (error) {
+    console.error("chatExist function failed");
+  }
+};
 export { createChat };
