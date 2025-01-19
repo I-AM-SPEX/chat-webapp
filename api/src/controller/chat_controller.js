19d79e920aa24e54752c228dc6a3f67c448c95b7
Samuel Williams <samuelspexy@gmail.com>
2025-01-19 22:06:45 +0200

    new chat controller

--- api/src/controller/chat_controller.js
+++ api/src/controller/chat_controller.js
@@ -1,21 +1,38 @@
 import { addFriendToDb } from "../config/db.js";
+import { createChat } from "../service/chat_service.js";
 
-const addFriend = async (req, res) => {
+//const addFriend = async (req, res) => {
+//  const { userName, friendUserName } = req.body;
+//  try {
+//    if (userName && friendUserName) {
+//      const result = await addFriendToDb(userName, friendUserName);
+//      if (result.status) {
+//        res.status(200).json({ message: result.message });
+//      } else {
+//        result.status(200).json({ message: result.message });
+//      }
+//    } else {
+//      res;
+//    }
+//  } catch (error) {
+//    console.log("AddFriend Controller Failed", error);
+//  }
+//};
+
+const newChat = async (req, res) => {
   const { userName, friendUserName } = req.body;
   try {
     if (userName && friendUserName) {
-      const result = await addFriendToDb(userName, friendUserName);
+      const result = await createChat(userName, friendUserName);
       if (result.status) {
         res.status(200).json({ message: result.message });
       } else {
-        result.status(200).json({ message: result.message });
+        res.status(200).json({ message: result.message });
       }
     } else {
-      res;
+      res.status(200).json({ message: "User names are undefined" });
     }
-  } catch (error) {
-    console.log("AddFriend Controller Failed", error);
-  }
+  } catch (error) { }
 };
-
-export { addFriend };
+const addMessage = async (req, res) => { };
+export { newChat, addMessage };
