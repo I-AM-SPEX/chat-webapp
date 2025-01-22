db6db7ee5db6bc5cff4a90c013b4b29ef8de3554
Samuel Williams <samuelspexy@gmail.com>
2025-01-22 12:42:24 +0200

    Friend Component

---
+++ client/src/components/Friend.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Friend.css";
+const Friend = () => {
+  return (
+    <div className="friend-container">
+      <p>Name</p>
+      <div>Online</div>
+    </div>
+  );
+};
+
+export default Friend;
