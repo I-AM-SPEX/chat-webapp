a0de824ed7795fb6f87252983b699da4a10a9117
Samuel Williams <samuelspexy@gmail.com>
2025-01-22 23:33:21 +0200

    Emit id for new Message in newMessage controller

--- api/src/controller/chat_controller.js
+++ api/src/controller/chat_controller.js
@@ -1,9 +1,11 @@
+import { io } from "../../main.js";
 import { addFriendToDb, getAllChats } from "../config/db.js";
 import {
   createChat,
   deliverMessage,
   getUserChats,
 } from "../service/chat_service.js";
+import { getRecipientId } from "../util/utils.js";
 
 //const addFriend = async (req, res) => {
 //  const { userName, friendUserName } = req.body;
@@ -44,6 +46,8 @@ const newMessage = async (req, res) => {
     if (message) {
       const result = await deliverMessage(chatId, message);
       if (result.status) {
+        console.log(getRecipientId(message.id, chatId));
+        io.emit("testing", getRecipientId(message.id, chatId));
         res.status(200).json({ message: result.message });
       } else {
         res.status(200).json({ message: result.message });
