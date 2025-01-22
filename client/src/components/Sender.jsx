83473fdf1890b53ec009dd836751b7c5771b82c5
Samuel Williams <samuelspexy@gmail.com>
2025-01-22 12:43:14 +0200

    Sender Component

---
+++ client/src/components/Sender.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Sender.css";
+const Sender = (props) => {
+  return (
+    <div className="sender-message-container">
+      <p>Message</p>
+      <p className="time-stamp">Time Stamp</p>
+    </div>
+  );
+};
+
+export default Sender;
