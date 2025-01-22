2e412c5f475363c0dccdeff2af1bd79b4d77f1cc
Samuel Williams <samuelspexy@gmail.com>
2025-01-22 12:44:31 +0200

    Receiver Component

---
+++ client/src/components/Receiver.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Receiver.css";
+const Receiver = () => {
+  return (
+    <div className="receiver-message-container">
+      <p>Message</p>
+      <p className="time-stamp">Time Stamp</p>
+    </div>
+  );
+};
+
+export default Receiver;
