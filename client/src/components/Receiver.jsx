941d2a743c2ba8ff5904acdb5b360ff19866aa8b
Samuel Williams <samuelspexy@gmail.com>
2025-01-22 18:08:59 +0200

    Removed console.log from Reciever

--- client/src/components/Receiver.jsx
+++ client/src/components/Receiver.jsx
@@ -1,8 +1,5 @@
 import "./Receiver.css";
 const Receiver = ({ message }) => {
-  console.log(message);
-  console.log("the props");
-
   return (
     <div className="receiver-message-container">
       <p>{message.text}</p>
